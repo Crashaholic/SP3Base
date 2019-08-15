88cdfe773e14db00332d63211a5026e4040bac16
Crashaholic <33598082+Crashaholic@users.noreply.github.com>
2019-08-15 09:26:54 +0800

    Minor: Position from Vertex.h can now be casted as a Vector3

--- Game/Source/Vertex.h
+++ Game/Source/Vertex.h
@@ -8,6 +8,7 @@ struct Position
 	float x, y, z;
 	Position(float x = 0, float y = 0, float z = 0) {Set(x, y, z);}
 	void Set(float x, float y, float z) {this->x = x; this->y = y; this->z = z;}
+	operator Vector3() { return Vector3(x, y, z); };
 };
 
 struct Color
