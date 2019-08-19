9801d66c3c087ce319862a332af8f8d3e935215e
HL <lhuiling302@gmail.com>
2019-08-19 17:07:15 +0800

    changed upgrade drop speed

--- Game/Source/GOManager.cpp
+++ Game/Source/GOManager.cpp
@@ -718,7 +718,7 @@ void GOManager::enemyDeath(GameObject * go)
 			switch (sceneID)
 			{
 			case STYPE::FROM_PLANE:
-				up->vel.y = 5.0f;
+				up->vel.y = 3.0f;
 				break;
 			case STYPE::FROM_TANK:
 				up->vel.y = -5.0f;
