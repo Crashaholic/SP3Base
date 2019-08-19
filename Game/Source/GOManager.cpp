806293d595b3926bc7e9124bec07cd556531b92d
Ryan Chan <ryanchan3000@gmail.com>
2019-08-19 11:06:12 +0800

    logger stuff and randomise player respawn.

--- Game/Source/GOManager.cpp
+++ Game/Source/GOManager.cpp
@@ -3,6 +3,7 @@
 #include "Mtx44.h"
 #include "GOManager.h"
 #include "SceneManager.h"
+#include "Logging.h"
 
 #include <stdlib.h>     /* srand, rand */
 #include <time.h>       /* time */
@@ -292,6 +293,7 @@ void GOManager::collisionResponse(GameObject * go1, GameObject * go2)
 		case GameObject::ENEMY_BUILDING:
 		{
 			toExplosion(go2);
+			LOG_TRACE("Player collided with enemy");
 
 			// check go1 again
 			switch (go1->type)
@@ -315,18 +317,21 @@ void GOManager::collisionResponse(GameObject * go1, GameObject * go2)
 		// Upgrade pickups
 		case GameObject::UPGRADE_1:
 		{
+			LOG_TRACE("Player picked up UPGRADE_1");
 			++upgrade_1;
 			go2->active = false;
 			break;
 		}
 		case GameObject::UPGRADE_2:
 		{
+			LOG_TRACE("Player picked up UPGRADE_2");
 			++upgrade_2;
 			go2->active = false;
 			break;
 		}
 		case GameObject::UPGRADE_3:
 		{
+			LOG_TRACE("Player picked up UPGRADE_3");
 			++planeLives;
 			++tankLives;
 			go2->active = false;
@@ -347,6 +352,7 @@ void GOManager::collisionResponse(GameObject * go1, GameObject * go2)
 	case GameObject::PLAYER_PROJECTILE_BOMB:
 	case GameObject::ENEMY_PROJECTILE_BOMB:
 	{
+		LOG_NONE("Projectile collided with object");
 		switch (go2->type)
 		{
 		case GameObject::ENEMY_PLANE_AGGRESSIVE:
@@ -372,6 +378,7 @@ void GOManager::collisionResponse(GameObject * go1, GameObject * go2)
 	}
 	case GameObject::PLAYER_PROJECTILE_NUKE:
 	{
+		LOG_NONE("Projectile collided with object");
 		go1->exRadius = 20.0f;
 		toExplosion(go1);
 		break;
@@ -379,6 +386,7 @@ void GOManager::collisionResponse(GameObject * go1, GameObject * go2)
 	case GameObject::PLAYER_PROJECTILE_MACHINE:
 	case GameObject::ENEMY_PROJECTILE_MACHINE:
 	{
+		LOG_NONE("Projectile collided with object");
 		go1->exRadius = 5.0f;
 		toExplosion(go1);
 		break;
@@ -428,6 +436,7 @@ void GOManager::terrainResponse(GameObject * go)
 	case GameObject::PLAYER_PLANE_KOMET:
 	case GameObject::PLAYER_PLANE_A10:
 	{
+		LOG_TRACE("Player collided with terrain");
 		planeDeath(go);
 		break;
 	}
@@ -435,6 +444,7 @@ void GOManager::terrainResponse(GameObject * go)
 	case GameObject::UPGRADE_2:
 	case GameObject::UPGRADE_3:
 	{
+		LOG_TRACE("UPGRADE collided with terrain");
 		if (go->vel.y < 0.0f)
 		{
 			go->vel.y = 0.0f;
@@ -446,12 +456,14 @@ void GOManager::terrainResponse(GameObject * go)
 	case GameObject::PLAYER_PROJECTILE_BOMB:
 	case GameObject::ENEMY_PROJECTILE_BOMB:
 	{
+		LOG_NONE("Projectile collided with terrain");
 		go->exRadius = 7.0f;
 		toExplosion(go);
 		break;
 	}
 	case GameObject::PLAYER_PROJECTILE_NUKE:
 	{
+		LOG_NONE("Projectile collided with terrain");
 		go->exRadius = 20.0f;
 		toExplosion(go);
 		break;
@@ -459,6 +471,7 @@ void GOManager::terrainResponse(GameObject * go)
 	case GameObject::PLAYER_PROJECTILE_MACHINE:
 	case GameObject::ENEMY_PROJECTILE_MACHINE:
 	{
+		LOG_NONE("Projectile collided with terrain");
 		go->exRadius = 5.0f;
 		toExplosion(go);
 		break;
@@ -472,28 +485,24 @@ void GOManager::terrainResponse(GameObject * go)
 
 void GOManager::planeDeath(GameObject * go)
 {
-	//if (go->Iframes <= 0.0)
+	LOG_ERROR("Player plane exploded");
+	--planeLives;
+	++tankKills;
+	GameObject* ex = fetchGO();
+	ex->exRadius = 10.0f;
+	ex->pos = go->pos;
+	go->active = false;
+	toExplosion(ex);
+	if (planeLives <= 0)
 	{
-		--planeLives;
-		++tankKills;
-		GameObject* ex = fetchGO();
-		ex->exRadius = 10.0f;
-		ex->pos = go->pos;
-		go->active = false;
-		toExplosion(ex);
-		//go->Iframes = 5.0;
-		if (planeLives <= 0)
-		{
-			// TODO: Ryan & Yan Quan
-			// Switch scene and pass high score to YQ's function
-			planeHighscore = planeKills + static_cast<int>(planeAccuracy / 10.0f) * planeKills;
-		}
-		else
-		{
-			go->reset();
-			go->Iframes = 5.0;
-			go->active = true;
-		}
+		planeHighscore = planeKills + static_cast<int>(planeAccuracy / 10.0f) * planeKills;
+	}
+	else
+	{
+		go->defaultPos.x = Math::RandFloatMinMax(10.0f, 166.0f);
+		go->reset();
+		go->Iframes = 5.0;
+		go->active = true;
 	}
 }
 
@@ -501,6 +510,7 @@ void GOManager::tankDeath(GameObject* go)
 {
 	if (go->Iframes <= 0.0)
 	{
+		LOG_ERROR("Player tank exploded");
 		--tankLives;
 		++planeKills;
 		GameObject* ex = fetchGO();
@@ -516,6 +526,7 @@ void GOManager::tankDeath(GameObject* go)
 		}
 		else
 		{
+			go->defaultPos.x = Math::RandFloatMinMax(10.0f, 166.0f);
 			go->reset();
 			go->Iframes = 5.0;
 			go->active = true;
--- Game/Source/Scene2Select.cpp
+++ Game/Source/Scene2Select.cpp
@@ -72,23 +72,19 @@ void Scene2Select::Update(double dt)
 	if (!bLButtonState && Application::IsMousePressed(0))
 	{
 		bLButtonState = true;
-		std::cout << "LBUTTON DOWN" << std::endl;
 	}
 	else if (bLButtonState && !Application::IsMousePressed(0))
 	{
 		bLButtonState = false;
-		std::cout << "LBUTTON UP" << std::endl;
 	}
 	static bool bRButtonState = false;
 	if (!bRButtonState && Application::IsMousePressed(1))
 	{
 		bRButtonState = true;
-		std::cout << "RBUTTON DOWN" << std::endl;
 	}
 	else if (bRButtonState && !Application::IsMousePressed(1))
 	{
 		bRButtonState = false;
-		std::cout << "RBUTTON UP" << std::endl;
 	}
 
 	// Physics Simulation Section
